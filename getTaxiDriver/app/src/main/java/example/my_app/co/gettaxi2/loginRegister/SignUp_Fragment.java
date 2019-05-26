9ba2e408262cc7bf553f615e341e78da991a8cf1
Itzik Friedman <itzikfriedman@gmail.com>
2019-05-26 21:36:19 +0300

    a flawfull start of firebase authentication

--- getTaxiDriver/app/src/main/java/example/my_app/co/gettaxi2/loginRegister/SignUp_Fragment.java
+++ getTaxiDriver/app/src/main/java/example/my_app/co/gettaxi2/loginRegister/SignUp_Fragment.java
@@ -1,13 +1,17 @@
 package example.my_app.co.gettaxi2.loginRegister;
+import java.util.concurrent.Executor;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.content.res.ColorStateList;
 import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,7 +22,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import example.my_app.co.gettaxi2.R;
+
+import static android.support.constraint.Constraints.TAG;
 
 
 public class SignUp_Fragment extends Fragment implements OnClickListener {
@@ -28,6 +40,7 @@ public class SignUp_Fragment extends Fragment implements OnClickListener {
     private static TextView login;
     private static Button signUpButton;
     private static CheckBox terms_conditions;
+    private FirebaseAuth mAuth;
     SharedPreferences sp;
 
 
@@ -47,7 +60,8 @@ public class SignUp_Fragment extends Fragment implements OnClickListener {
 
     // Initialize all views
     private void initViews() {
-
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         fullName = (EditText) view.findViewById(R.id.fullName);
         emailId = (EditText) view.findViewById(R.id.userEmailId);
@@ -59,12 +73,6 @@ public class SignUp_Fragment extends Fragment implements OnClickListener {
         login = (TextView) view.findViewById(R.id.already_user);
         terms_conditions = (CheckBox) view.findViewById(R.id.terms_conditions);
         sp = this.getActivity().getSharedPreferences("driver", Context.MODE_PRIVATE);
-
-
-
-
-
-
 
         // Setting text selector over textviews
         XmlResourceParser xrp = getResources().getXml(R.drawable.text_selector);
@@ -145,8 +153,9 @@ public class SignUp_Fragment extends Fragment implements OnClickListener {
 
             // Else do signup or do your stuff
         else {
-            Toast.makeText(getActivity(), "Do SignUp.", Toast.LENGTH_SHORT)
-                    .show();
+            createAccount(getEmailId, getPassword);
+
+            Toast.makeText(getActivity(), "Do SignUp.", Toast.LENGTH_SHORT).show();
             SharedPreferences.Editor editor = sp.edit();
             editor.putString("name", fullName.getText().toString());
             editor.putString("email", emailId.getText().toString());
@@ -154,6 +163,36 @@ public class SignUp_Fragment extends Fragment implements OnClickListener {
             editor.commit();
         }
 
+    }
+
+    private void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                        }
+                    }
+                });}
+               // .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+               //     @Override
+               //     public void onComplete(@NonNull Task<AuthResult> task) {
+               //         if (task.isSuccessful()) {
+               //             // Sign in success, update UI with the signed-in user's information
+               //             Log.d(TAG, "createUserWithEmail:success");
+               //             FirebaseUser user = mAuth.getCurrentUser();
+               //             //updateUI(user);
+               //         } else {
+               //             // If sign in fails, display a message to the user.
+               //             Log.w(TAG, "createUserWithEmail:failure", task.getException());
+//
+               //             //updateUI(null);
+               //         }
+//
+               //         // ...
+               //     }
+               // });
     }
 
 
